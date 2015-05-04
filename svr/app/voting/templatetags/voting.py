05f06aecd97c06a0f15248b408ee3211e6f44188
marcelkornblum <me@marcelkornblum.com>
2015-05-04 16:15:54 +0100

    improved voting widget markup for easier styling

--- svr/app/voting/templatetags/voting.py
+++ svr/app/voting/templatetags/voting.py
@@ -4,6 +4,14 @@ register = template.Library()
 
 @register.inclusion_tag('vote_widget.html', takes_context=True)
 def vote_widget(context, generic_object):
+    existing_upvote = False
+    existing_downvote = False
+    existing_user_vote = generic_object.get_vote_for_user(context['request'].user)
+    if existing_user_vote is not None:
+        if existing_user_vote.is_upvote is True:
+            existing_upvote = True
+        else:
+            existing_downvote = True
     app_name = generic_object._meta.app_label
     model_name = generic_object._meta.model_name
     object_id = generic_object.pk
@@ -13,5 +21,7 @@ def vote_widget(context, generic_object):
         'app_name': app_name,
         'model_name': model_name,
         'object_id': object_id,
-        'votes': votes
+        'votes': votes,
+        'existing_upvote': existing_upvote,
+        'existing_downvote': existing_downvote,
     }
