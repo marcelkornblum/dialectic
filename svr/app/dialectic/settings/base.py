4416c427f90ecf5843a0c5f2311d2206b6b43a12
marcelkornblum <me@marcelkornblum.com>
2015-05-07 02:04:06 +0100

    removed a ccidental heroku settings

--- svr/app/dialectic/settings/base.py
+++ svr/app/dialectic/settings/base.py
@@ -384,26 +384,3 @@ COMMENTS_APP = 'fluent_comments'
 
 
 
-
-<<<<<<< HEAD
-# from https://devcenter.heroku.com/articles/getting-started-with-django
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-=======
->>>>>>> d707f7b... commit to test heroku build - 03
